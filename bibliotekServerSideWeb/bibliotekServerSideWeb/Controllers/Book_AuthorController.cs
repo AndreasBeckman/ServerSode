e474a174820189326210ac02992b496ed119795e
janenilsson <Janejiranilsson@wlanguest141131.eduroam.hj.se>
2015-03-10 14:21:20 +0100

    klasser

---
+++ bibliotekServerSideWeb/bibliotekServerSideWeb/Controllers/Book_AuthorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bibliotekServerSideWeb.Controllers
+{
+    public class Book_AuthorController : Controller
+    {
+        //
+        // GET: /Book_Author/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
